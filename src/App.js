1c396bb12d203beb5264474513c9351c9c585656
Sese <sese.miguel9@gmail.com>
2020-07-02 21:03:27 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    render() {
+        return (
+            <>
+                <Header />
+                <MainContent />
+                <Footer />
+            </>
+        )
+
+    }
+
 }
 
-export default App;
+class Header extends React.Component {
+    render() {
+        return (
+            <>
+                <header>This is my header</header>
+            </>
+        )
+    }
+}
+
+class MainContent extends React.Component {
+    render() {
+        return (
+            <>
+                <p>This is the main content</p>
+            </>
+        )
+    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <>
+                <footer>This is the footer</footer>
+            </>
+        )
+    }
+}
+
+export default App
